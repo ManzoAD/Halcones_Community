0751c6294942dbe2b05c33ef18dcf4a0a4a30486
ManzoAD <95952704+ManzoAD@users.noreply.github.com>
2022-12-17 12:46:04 -0600

    Delete login directory

--- login/__init__.py
+++
@@ -1,8 +0,0 @@
-# El archivo __init__ es constructor del módulo de categorias
-from flask import Blueprint
-
-# Definir el Blueprint
-login = Blueprint('login', __name__, url_prefix='/iniciar_sesion', template_folder='templates')
-
-# Le estamos diciendo al blueprint que tiene rutas definidas
-from . import routes
