cbe24f15c71f4d2b8085b9e66ab2b248f0299a01
ManzoAD <95952704+ManzoAD@users.noreply.github.com>
2022-12-17 12:46:10 -0600

    Delete registro directory

--- registro/__init__.py
+++
@@ -1,8 +0,0 @@
-# El archivo __init__ es constructor del módulo de categorias
-from flask import Blueprint
-
-# Definir el Blueprint
-registro = Blueprint('registro', __name__, url_prefix='/registros', template_folder='templates')
-
-# Le estamos diciendo al blueprint que tiene rutas definidas
-from . import routes
